c23c24e6714995045955ea9841c7a7a0f640c8f1
ross-brown <rbrown_98@yahoo.com>
2023-08-25 15:56:37 -0500

    adjust assert in homepage check to just look for <table

--- test_app.py
+++ test_app.py
@@ -26,8 +26,7 @@ class BoggleAppTestCase(TestCase):
             html = response.get_data(as_text=True)
 
             self.assertEqual(response.status_code, 200)
-            self.assertIn('<table class="board">', html)
-
+            self.assertIn('<table', html)
 
     def test_api_new_game(self):
         """Test starting a new game."""
