acfc376bef67979d475441068d6c52a6bb6f0083
ross-brown <rbrown_98@yahoo.com>
2023-08-25 14:18:12 -0500

    add doctests for check_word method

--- wordlist.py
+++ wordlist.py
@@ -28,7 +28,18 @@ class WordList:
         return words
 
     def check_word(self, word):
-        """Is word in word list?"""
+        """Is word in word list?
+
+        >>> wl = WordList("test_dictionary.txt")
+        >>> wl.check_word("CAT")
+        True
+
+        >>> wl = WordList("test_dictionary.txt")
+        >>> wl.check_word("FHDSK")
+        False
+
+
+        """
 
         return word in self.words
 
